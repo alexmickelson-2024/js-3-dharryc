30edbddaf0247484f11ff22b2baf3b6435be5621
dharryc <dharrisoncook@gmail.com>
2024-10-07 13:41:09 -0600

    I think it's finished?

--- site.js
+++ site.js
@@ -1,23 +1,51 @@
-import { animals } from "./animals.js";
-const cardContainer = document.getElementById("card-container-id");
-console.log(cardContainer);
-console.log(animals[0]);
-const makeCards = () => {
+import { animals } from "./animals.js"; //getting the animals array
+const cardContainer = document.getElementById("card-container-id"); //getting the card container node
 
+//pulling filterwords from the url
+function getWords() {
+  const s = window.location.href;
+  if (s.includes("filter=")) {
+    const array = s.split("?");
+    const mediaArray = array[1].split("&");
+    let filters = mediaArray.find((media) => {
+      return media.includes("filter=");
+    });
+    const filterWords = filters.split("=")[1];
+    return filterWords;
+  } else {
+    return "";
+  }
 }
+//returning them as individual words
+let filterWords = getWords().split("%20");
+for (let i = 0; i < filterWords.length; i++) {
+  filterWords[i] = filterWords[i].toLowerCase();
+}
+console.log(filterWords);
+
+function checkFilters(card) {
+  if(filterWords.length > 0){
+    for (let i = 0; i < filterWords.length; i++) {
+      if(card.description.toLowerCase().includes(filterWords[i])) return true;
+    }
+  } else return false;
+}
+
+
+// building the actual cards
 // I chose to use let here because I have to add to the value to build the cards
 let cardConstructor = ``;
 for (let i = 0; i < animals.length; i++) {
+  if (checkFilters(animals[i])) {
+
     cardConstructor += `<div class="card">
-              <img src="${animals[i].image}" alt="${animals[i].title}" />
-              <div class="card-text">
-                <h3>${animals[i].title}</h3>
-                <p>${animals[i].description}
-                </p>
-              </div>
-            </div>`;
+    <img src="${animals[i].image}" alt="${animals[i].title}" />
+    <div class="card-text">
+    <h3>${animals[i].title}</h3>
+    <p>${animals[i].description}
+    </p>
+    </div>
+    </div>`;
+  }
 }
 cardContainer.innerHTML = cardConstructor;
-const s = window.location.href;
-const a = s.split("%20");
-console.log(a);
