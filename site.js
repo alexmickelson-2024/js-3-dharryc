33bb618ac53c910e426a22a2f43dd39cf819b5c8
dharryc <dharrisoncook@gmail.com>
2024-10-07 13:50:14 -0600

    finished this time

--- site.js
+++ site.js
@@ -1,5 +1,6 @@
 import { animals } from "./animals.js"; //getting the animals array
 const cardContainer = document.getElementById("card-container-id"); //getting the card container node
+cardContainer.replaceChildren(); //remove the existing cards
 
 //pulling filterwords from the url
 function getWords() {
@@ -17,16 +18,18 @@ function getWords() {
   }
 }
 //returning them as individual words
+//I'm assuming here that each filter word is separated by a space, rather than some other character because that's how it was in the example.
 let filterWords = getWords().split("%20");
 for (let i = 0; i < filterWords.length; i++) {
   filterWords[i] = filterWords[i].toLowerCase();
 }
 console.log(filterWords);
 
+//function to check if a given card contains the filterword
 function checkFilters(card) {
-  if(filterWords.length > 0){
+  if (filterWords.length > 0) {
     for (let i = 0; i < filterWords.length; i++) {
-      if(card.description.toLowerCase().includes(filterWords[i])) return true;
+      if (card.description.toLowerCase().includes(filterWords[i]) || card.title.toLowerCase().includes(filterWords[i])) return true;
     }
   } else return false;
 }
@@ -36,8 +39,8 @@ function checkFilters(card) {
 // I chose to use let here because I have to add to the value to build the cards
 let cardConstructor = ``;
 for (let i = 0; i < animals.length; i++) {
+  //make sure the animal we're looking at actually has our filter words in it
   if (checkFilters(animals[i])) {
-
     cardConstructor += `<div class="card">
     <img src="${animals[i].image}" alt="${animals[i].title}" />
     <div class="card-text">
