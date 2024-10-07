c22fed1e12c4a2f785e8c065e805049d0a484321
dharryc <dharrisoncook@gmail.com>
2024-10-07 13:14:34 -0600

    rendered cards

--- site.js
+++ site.js
@@ -1,5 +1,23 @@
 import { animals } from "./animals.js";
-for(let i = 0; i < animals.length; i++){
+const cardContainer = document.getElementById("card-container-id");
+console.log(cardContainer);
+console.log(animals[0]);
+const makeCards = () => {
 
-    console.log(animals[i]); // example showing you can use the animals array
 }
+// I chose to use let here because I have to add to the value to build the cards
+let cardConstructor = ``;
+for (let i = 0; i < animals.length; i++) {
+    cardConstructor += `<div class="card">
+              <img src="${animals[i].image}" alt="${animals[i].title}" />
+              <div class="card-text">
+                <h3>${animals[i].title}</h3>
+                <p>${animals[i].description}
+                </p>
+              </div>
+            </div>`;
+}
+cardContainer.innerHTML = cardConstructor;
+const s = window.location.href;
+const a = s.split("%20");
+console.log(a);
